e2b7e930193478662dc01e5ab2ffaa324a8c4a62
ChathuniMadhusha <chathunionline97@gmail.com>
2024-10-17 16:49:31 +0530

    new
    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react';
-
+import BlogList from './BlogList';
 
 
 const Home = () => {
-    const [blog, setBlogs] = useState([
+    const [blogs, setBlogs] = useState([
         {title: 'My new website', body:'Lorem ipsum...', author:'mario', id:1},
         {title: 'Welcome party!', body:'Lorem ipsum...', author:'yoshi', id:2},
         {title: 'Web dev top tips', body:'Lorem ipsum...', author:'mario', id:3},
     ]);
+
+    // delete blog
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+
+
+
 // let name = 'chathu';
 // const [name, setName] = useState('chathu');
 // const [age, setAge] = useState(27);
@@ -27,12 +37,20 @@ const Home = () => {
 
     return (
         <div className="home">
-            {blog.map((blog)=>(
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+
+
+
+
+
+
+
+            {/* list eken filter krla pennana
+            <BlogList blogs={blogs.filter((blog)=> blog.author=== 'mario')} title="mario's blogs" /> */}
+            
+            
+            
+            
             {/* <h2>Homepage</h2>
             <p>{ name } is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
