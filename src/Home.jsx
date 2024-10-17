aa26c5210a5e569c453af1600f3c2b921adf418d
ChathuniMadhusha <chathunionline97@gmail.com>
2024-10-17 10:14:22 +0530

    Update Home.jsx
    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,31 @@
+import { useState } from 'react';
+
+
+
 const Home = () => {
+// let name = 'chathu';
+const [name, setName] = useState('chathu');
+const [age, setAge] = useState(27);
 
-const handleClick = (e) => {
-        console.log('hello', e)
+const handleClick = () => {
+        setName('mario');
+        setAge(25);
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name , e.target)
-    }
+    // const handleClick = (e) => {
+    //     console.log('hello', e)
+    // }
+
+    // const handleClickAgain = (name, e) => {
+    //     console.log('hello ' + name , e.target)
+    // }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('chathu', e)}>Click me Again</button>
+            {/* <button onClick={(e) => handleClickAgain('chathu', e)}>Click me Again</button> */}
         </div>
     );
 }
