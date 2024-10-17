2556ecc7928d80b5925d2413e2330def24c777cd
ChathuniMadhusha <chathunionline97@gmail.com>
2024-10-17 18:11:27 +0530

    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 
@@ -9,13 +9,18 @@ const Home = () => {
         {title: 'Web dev top tips', body:'Lorem ipsum...', author:'mario', id:3},
     ]);
 
+    const [name, setName] = useState('mario');
+
     // delete blog
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
-
+    useEffect(() => {
+            console.log('blaaaa');
+            console.log(name);
+    },[name]);
 
 
 // let name = 'chathu';
@@ -38,13 +43,8 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-
-
-
-
-
-
-
+            <button onClick={() => setName('luigi')}>Change name</button>
+            <p>{name}</p>
             {/* list eken filter krla pennana
             <BlogList blogs={blogs.filter((blog)=> blog.author=== 'mario')} title="mario's blogs" /> */}
             
